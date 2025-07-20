cd112ddd3f2461d909c7b3717145ba2c9d5a8b45
Dhiraj Singh <er.dhirajsingh93@gmail.com>
2025-07-20 20:42:40 +0530

    Update main.tf

--- environment/preprod/main.tf
+++ environment/preprod/main.tf
@@ -6,32 +6,6 @@ module "resource_group" {
 
 }
 
-module "resource_group" {
-  source = "../../modules/azurerm_resource_group"
-
-  resource_group_name     = var.resource_group_name2
-  resource_group_location = var.resource_group_location2
-}
-
-module "resource_group97" {
-  source = "../../modules/azurerm_resource_group"
-
-  resource_group_name     = var.resource_group_name97
-  resource_group_location = var.resource_group_location97
-
-module "resource_group99" {
-  source = "../../modules/azurerm_resource_group"
-
-  resource_group_name     = var.resource_group_name99
-  resource_group_location = var.resource_group_location99
-}
-module "resource_group87" {
-  source = "../../modules/azurerm_resource_group"
-
-  resource_group_name     = var.resource_group_name87
-  resource_group_location = var.resource_group_location87
-}
-
 module "key_vault" {
   source                  = "../../modules/azurerm_key_vault"
   depends_on              = [module.resource_group]
